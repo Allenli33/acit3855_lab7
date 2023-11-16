ad3efc6c07da02ae2390a40a83819c69b0ae7d76
Hao Li <hli197@my.bcit.ca>
2023-11-16 12:36:59 -0800

    updated storage app.py.

--- storage/app.py
+++ storage/app.py
@@ -16,10 +16,30 @@ from pykafka.common import OffsetType
 from threading import Thread
 from sqlalchemy import and_
 import time
+import os
 
-# load the app config file
-with open('app_conf.yml', 'r') as f:
+# Check environment and set configuration file paths
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+# Load application configuration
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 # Access specific configuration settings
 db_user = app_config['datastore']['user']
@@ -28,14 +48,7 @@ db_hostname = app_config['datastore']['hostname']
 db_port = app_config['datastore']['port']
 db_name = app_config['datastore']['db']
 
-# load the log config file
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
-
-# Create a logger from the basicLogger defined in the configuration file. Make sure to import the logging
-# and logging.config modules.
-logger = logging.getLogger('basicLogger')
+# Create database connection
 
 DB_ENGINE = create_engine(
     f'mysql+pymysql://{db_user}:{db_password}@{db_hostname}:{db_port}/{db_name}')
