d59e10d8a4b9846fb13f2b233ef06a724ef2bf65
Hao Li <hli197@my.bcit.ca>
2023-11-15 23:02:42 -0800

    updated endpointaudit.js.

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -5,6 +5,7 @@ export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null);
+    const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
     const [index, setIndex] = useState(null);
 
     useEffect(() => {
