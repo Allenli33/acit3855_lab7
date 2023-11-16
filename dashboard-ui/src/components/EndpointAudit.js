b0d4d10d74d9eb30e523a96dc3d0e7bb408af13b
Hao Li <hli197@my.bcit.ca>
2023-11-15 21:20:47 -0800

    updated endpoitaudit.js.

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -10,7 +10,7 @@ export default function EndpointAudit(props) {
     setIndex(rand_val);
 
     const getAudit = () => {
-        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
+        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com:8110${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result) => {
                 console.log("Received Audit Results for " + props.endpoint)
