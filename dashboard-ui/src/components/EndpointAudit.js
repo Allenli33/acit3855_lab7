3f590974d781c0617cf0b81490f0b6fb13bfb215
Hao Li <hli197@my.bcit.ca>
2023-11-15 23:39:04 -0800

    updated endpointaudit.js.

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -1,30 +1,26 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
 export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null);
-    const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
     const [index, setIndex] = useState(null);
 
-    useEffect(() => {
-        // Set index when component mounts
-        setIndex(rand_val);
-    }, []);
-
-    const getAudit = useCallback(() => {
-        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com:8110${props.endpoint}?index=${index}`)
+    const getAudit = () => {
+        const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com:8110${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result) => {
-                console.log("Received Audit Results for " + props.endpoint)
+                console.log("Received Audit Results for " + props.endpoint);
                 setLog(result);
+                setIndex(rand_val); // Set index here after successful fetch
                 setIsLoaded(true);
             }, (error) => {
-                setError(error)
+                setError(error);
                 setIsLoaded(true);
-            })
-    }, [props.endpoint, index]); // Added index to the dependency array
+            });
+    };
 
     useEffect(() => {
         const interval = setInterval(getAudit, 4000); // Update every 4 seconds
@@ -32,9 +28,9 @@ export default function EndpointAudit(props) {
     }, [getAudit]);
 
     if (error) {
-        return (<div className={"error"}>Error found when fetching from API</div>);
+        return <div className={"error"}>Error found when fetching from API</div>;
     } else if (!isLoaded) {
-        return (<div>Loading...</div>);
+        return <div>Loading...</div>;
     } else {
         return (
             <div>
