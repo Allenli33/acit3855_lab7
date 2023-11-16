eca9a78f198d64c33e0014eb2b512daabdf384b1
Hao Li <hli197@my.bcit.ca>
2023-11-15 21:45:03 -0800

    updated receiver app.py.

--- receiver/app.py
+++ receiver/app.py
@@ -1,5 +1,6 @@
 import connexion
 from connexion import NoContent
+import requests
 import yaml
 import logging
 import logging.config
@@ -9,27 +10,40 @@ import json
 from pykafka import KafkaClient
 import time
 
-# Load the app config file
+# load the app config file
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-# Load the log config file
+# load the log config file
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
+# Create a logger from the basicLogger defined in the configuration file. Make sure to import the logging
+# and logging.config modules.
 logger = logging.getLogger('basicLogger')
 
 # Initialize KafkaClient at startup
-try:
-    logger.info("Initializing Kafka client")
-    kafka_client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
-    kafka_topic = kafka_client.topics[str.encode(app_config['events']['topic'])]
-    producer = kafka_topic.get_sync_producer()
-except Exception as e:
-    logger.error(f"Failed to initialize Kafka client: {str(e)}")
-    raise e
+def get_kafka_client():
+    retry_count = 0
+    max_retries = app_config['kafka']['max_retries']
+    sleep_time = app_config['kafka']['sleep_time']
 
+    while retry_count < max_retries:
+        try:
+            logger.info(f"Trying to connect to Kafka, attempt {retry_count+1}")
+            client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
+            return client
+        except Exception as e:
+            logger.error(f"Connection to Kafka failed: {str(e)}")
+            time.sleep(sleep_time)
+            retry_count += 1
+    raise Exception("Failed to connect to Kafka after retries")
+
+kafka_client = get_kafka_client()
+kafka_topic = kafka_client.topics[str.encode(app_config['events']['topic'])]
+producer = kafka_topic.get_sync_producer()
+    
 def send_to_kafka(event_type, payload):
     msg = {
         "type": event_type,
@@ -39,21 +53,55 @@ def send_to_kafka(event_type, payload):
     msg_str = json.dumps(msg)
     producer.produce(msg_str.encode('utf-8'))
 
+
+# Function to handle borrowing a book
 def borrow_book(body):
+
+    # Generate a unique trace_id
     trace_id = str(uuid.uuid4())
-    logger.info(f"Received event borrow_book request with a trace id of {trace_id}")
+
+    # Log the receipt of the event request
+    logger.info(
+        f"Received event borrow_book request with a trace id of {trace_id}")
+
+    # save the newely generated trace_id to the body
     body['trace_id'] = trace_id
+
+    # get the request data
     send_to_kafka("borrow_book", body)
+
+    # Log the return of the event response
+    # Log the successful event push
     logger.info(f"Pushed borrow_book event to Kafka (Id: {trace_id})")
+
+    # return a 201 status code
     return NoContent, 201
 
+# Function to handle returning a book
+
+
 def return_book(body):
+
+    # Generate a unique trace_id
     trace_id = str(uuid.uuid4())
-    logger.info(f"Received event return_book request with a trace id of {trace_id}")
+
+    # Log the receipt of the event request
+    logger.info(
+        f"Received event return_book request with a trace id of {trace_id}")
+
+    # save the newely generated trace_id to the body
     body['trace_id'] = trace_id
+
+    # Create a structured request_data sentence for returning
     send_to_kafka("return_book", body)
+
+    # Log the return of the event response with the trace_id
+    # Log the successful event push
+
     logger.info(f"Pushed return_book event to Kafka (Id: {trace_id})")
+    # return a 201 status code
     return NoContent, 201
+
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
