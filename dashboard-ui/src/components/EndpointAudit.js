419e72d92210dc949116e11afd7f8ff5884cc9aa
Hao Li <hli197@my.bcit.ca>
2023-11-15 22:53:06 -0800

    updated endpointaudit.js.

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react';
 import '../App.css';
 
 export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
-    const [error, setError] = useState(null)
-    const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const [error, setError] = useState(null);
     const [index, setIndex] = useState(null);
-    setIndex(rand_val);
 
-    const getAudit = () => {
-        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com:8110${props.endpoint}?index=${rand_val}`)
+    useEffect(() => {
+        // Set index when component mounts
+        setIndex(Math.floor(Math.random() * 100));
+    }, []);
+
+    const getAudit = useCallback(() => {
+        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com:8110${props.endpoint}?index=${index}`)
             .then(res => res.json())
             .then((result) => {
                 console.log("Received Audit Results for " + props.endpoint)
@@ -20,25 +23,23 @@ export default function EndpointAudit(props) {
                 setError(error)
                 setIsLoaded(true);
             })
-    }
+    }, [props.endpoint, index]); // Added index to the dependency array
+
     useEffect(() => {
-        const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
+        const interval = setInterval(getAudit, 4000); // Update every 4 seconds
         return () => clearInterval(interval);
     }, [getAudit]);
 
     if (error) {
-        return (<div className={"error"}>Error found when fetching from API</div>)
-    } else if (isLoaded === false) {
-        return (<div>Loading...</div>)
-    } else if (isLoaded === true) {
-
+        return (<div className={"error"}>Error found when fetching from API</div>);
+    } else if (!isLoaded) {
+        return (<div>Loading...</div>);
+    } else {
         return (
             <div>
                 <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
-        )
-
-
+        );
     }
 }
