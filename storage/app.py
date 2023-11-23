279e0c289dda2183f2f78c5d3e349bfe2aacde72
Hao Li <hli197@my.bcit.ca>
2023-11-23 10:02:04 -0800

    updated app.py for storage.

--- storage/app.py
+++ storage/app.py
@@ -249,7 +249,7 @@ def get_return_records_by_timestamp(timestamp, end_timestamp):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml", base_path="/storage", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     t1 = Thread(target=process_messages)
