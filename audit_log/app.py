9e5dabd0cd78db033134d5931b9f57525b9d5b20
Hao Li <hli197@my.bcit.ca>
2023-11-23 10:19:42 -0800

    updated app.py for audit_lod.

--- audit_log/app.py
+++ audit_log/app.py
@@ -129,8 +129,9 @@ def get_return_record_index(index):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", base_path="/audit_log" strict_validation=True, validate_responses=True)
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     app.run(port=8110)
