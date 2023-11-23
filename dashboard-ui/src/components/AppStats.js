6de6c35c24b01cefe42e1eb43a00e5807a654559
Hao Li <hli197@my.bcit.ca>
2023-11-23 13:13:58 -0800

    updated endpointaudit.js and appstats.js.

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -8,7 +8,7 @@ export default function AppStats() {
 
     const getStats = () => {
 
-        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com/stats`)
+        fetch(`http://allenliacit3855.eastus.cloudapp.azure.com/processing/stats`)
             .then(res => res.json())
             .then((result) => {
                 console.log("Received Stats")
